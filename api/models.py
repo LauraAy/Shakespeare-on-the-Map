0a4f94032b97978ac71cb1e77736a87d09e87ffc
laura_ay <laura.ay1564@gmail.com>
2025-06-13 15:14:08 -0700

    models set up

--- api/models.py
+++ api/models.py
@@ -1,54 +1,57 @@
 from django.db import models
+from django.contrib.gis.db import models
 
-class Play (models.Model)
-title = models.CharField(max_length=250)
-author = models.CharField(max_length=250)
-pub_place = models.CharField(max_length=250)
-pub_year = models.IntegerField(max_length=4)
-pub_date = models.CharField(max_length=250)
-publisher = models.CharField(max_length=250)
-editor1 = models.CharField(max_length=250)
-editor2 = models.CharField(max_length=250)
-editor3 = models.CharField(max_length=250)
-edition_num = models.IntegerField(max_length=4)
-edition_name = models.CharField(max_length=250)
+class Play (models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    pub_place = models.CharField(max_length=250)
+    pub_year = models.IntegerField(max_length=4)
+    pub_date = models.CharField(max_length=250)
+    publisher = models.CharField(max_length=250)
+    editor1 = models.CharField(max_length=250)
+    editor2 = models.CharField(max_length=250)
+    editor3 = models.CharField(max_length=250)
+    edition_num = models.IntegerField(max_length=4)
+    edition_name = models.CharField(max_length=250)
 
-class Quote (models.Model)
-word = models.CharField(max_length=250)
-word_id = models.CharField(max_length=250)
-quote = models.textField
-act = models.IntegerField(max_length=4)
-scene = models.IntegerField(max_length=4)
-line = models.IntegerField(max_length=4)
-line_id = models.CharField(max_length=250)
-quote_type = models.IntegerField(max_length=4)
-notes = models.textField
+class Quote (models.Model):
+    word = models.CharField(max_length=250)
+    word_id = models.CharField(max_length=250)
+    quote = models.textField()
+    act = models.IntegerField(max_length=4)
+    scene = models.IntegerField(max_length=4)
+    line = models.IntegerField(max_length=4)
+    line_id = models.CharField(max_length=250)
+    quote_type = models.IntegerField(max_length=4)
+    notes = models.textField()
 
-class Place_Real (models.Model)
-name_mod = models.CharField(max_length=250)
-name_alt = models.CharField(max_length=250)
-lat = 
-long =
-poly = 
-notes = models.textField
-place_type = models.CharField(max_length=250)
+class Place_Real (models.Model):
+    name_mod = models.CharField(max_length=250)
+    name_alt = models.CharField(max_length=250)
+    lon = models.FloatField()
+    lat = models.FloatField()
+    notes = models.textField()
+    place_type = models.CharField(max_length=250)
+    poly = models.PolygonField()
+    mpoly = models.MultiPolygonField()
 
-class Place_Fictional (models.Model)
-Name_Standard = models.CharField(max_length=250)
-Name_Alt = models.CharField(max_length=250)
-Notes = models.textField
+class Place_Fictional (models.Model):
+    Name_Standard = models.CharField(max_length=250)
+    Name_Alt = models.CharField(max_length=250)
+    Notes = models.textField
 
-class Place_Historical (models.Model)
-name_hist = models.CharField(max_length=250)
-name_alt = models.CharField(max_length=250)
-lat = 
-long =
-poly = 
-notes = models.textField
-place_type = models.CharField(max_length=250)
+class Place_Historical (models.Model):
+    name_hist = models.CharField(max_length=250)
+    name_alt = models.CharField(max_length=250)
+    lon = models.FloatField()
+    lat = models.FloatField()
+    notes = models.textField()
+    place_type = models.CharField(max_length=250)
+    poly = models.MultiPolygonField()
+    mpoly = models.MultiPolygonField()
 
 class Image (models.Model):
-image_file_name = models.CharField(max_length=250)
-image_file_size = models.CharField(max_length=250)
-image_id = models.CharField(max_length=250)
-image_ext = models.CharField(max_length=250)
+    image_file_name = models.CharField(max_length=250)
+    image_file_size = models.CharField(max_length=250)
+    image_id = models.CharField(max_length=250)
+    image_ext = models.CharField(max_length=250)
